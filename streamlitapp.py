116da4ce7595243dce9125c195d3865e9c55ebaa
Afsanay <priyanshusobti02@gmail.com>
2023-06-27 01:24:02 +0530

    added opts

--- streamlitapp.py
+++ streamlitapp.py
@@ -18,41 +18,44 @@ with st.sidebar:
         "This is an image to image model that changes the characteristics of an image. In this case this is able to color the drawings of anime characters")
 
 st.title("Anime Coloring with Pix to Pix GAN ShowCase")
-options = os.listdir(os.path.join('anime', 'images'))
-selected_photo = st.selectbox("Choose a photo", options=options)
 
-img_path = os.path.join('anime', 'images', selected_photo)
-image = np.array(Image.open(img_path))
+model_type = st.selectbox("Choose a model" , options=["anime","maps"])
+if model_type == "anime":
+    options = os.listdir(os.path.join('anime', 'images'))
+    selected_photo = st.selectbox("Choose a photo", options=options)
 
-input_image = image[:512, 512:, :3]
-target_image = image[:512, 512:, :3]
+    img_path = os.path.join('anime', 'images', selected_photo)
+    image = np.array(Image.open(img_path))
 
-augmentations = config.both_transform(image=input_image, image0=target_image)
-input_image = augmentations["image"]
-target_image = augmentations["image0"]
+    input_image = image[:512, 512:, :3]
+    target_image = image[:512, 512:, :3]
 
-target_image = config.transform_only_mask(image=target_image)["image"]
-input_image = input_image[None, :, :, :]
-target_image = target_image[None, :, :, :]
+    augmentations = config.both_transform(image=input_image, image0=target_image)
+    input_image = augmentations["image"]
+    target_image = augmentations["image0"]
 
-model = load_model()
-model.eval()
+    target_image = config.transform_only_mask(image=target_image)["image"]
+    input_image = input_image[None, :, :, :]
+    target_image = target_image[None, :, :, :]
 
-col1, col2 = st.columns(2)
-if options:
-    with col1:
-        img_path = os.path.join('anime', 'images', selected_photo)
-        image = np.array(Image.open(img_path))
+    model = load_model()
+    model.eval()
 
-        input_image = image[:512, 512:, :3]
-        input_image = input_image[None, :, :, :]
-        st.image(input_image)
+    col1, col2 = st.columns(2)
+    if options:
+        with col1:
+            img_path = os.path.join('anime', 'images', selected_photo)
+            image = np.array(Image.open(img_path))
 
-    with col2:
-        with torch.no_grad():
-            y_fake = model(target_image)
-            y_fake = y_fake * 0.5 + 0.5
-            y = torch.squeeze(y_fake)
-            fig, ax = plt.subplots()
-            ax = plt.imshow(y.permute(1, 2, 0))
-            st.pyplot(fig)
+            input_image = image[:512, 512:, :3]
+            input_image = input_image[None, :, :, :]
+            st.image(input_image)
+
+        with col2:
+            with torch.no_grad():
+                y_fake = model(target_image)
+                y_fake = y_fake * 0.5 + 0.5
+                y = torch.squeeze(y_fake)
+                fig, ax = plt.subplots()
+                ax = plt.imshow(y.permute(1, 2, 0))
+                st.pyplot(fig)
