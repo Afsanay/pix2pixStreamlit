2f83bf7599086d67b3920bce6551e41b9778aeb1
Afsanay <priyanshusobti02@gmail.com>
2023-06-27 02:40:07 +0530

    mapsaddded

--- streamlitapp.py
+++ streamlitapp.py
@@ -36,7 +36,7 @@ if model_type == "anime":
     target_image = config.transform_only_mask(image=target_image)["image"]
     input_image = input_image[None, :, :, :]
     target_image = target_image[None, :, :, :]
-    path = "gen.pth.tar"
+    path = "./anime_model/gen.pth.tar"
     model = load_model(path)
     model.eval()
 
@@ -58,3 +58,44 @@ if model_type == "anime":
                 fig, ax = plt.subplots()
                 ax = plt.imshow(y.permute(1, 2, 0))
                 st.pyplot(fig)
+
+elif model_type == "maps":
+    options = os.listdir(os.path.join('maps', 'images'))
+    selected_photo = st.selectbox("Choose a photo", options=options)
+
+    img_path = os.path.join('maps', 'images', selected_photo)
+    image = np.array(Image.open(img_path))
+
+    input_image = image[:600, :600, :3]
+    target_image = image[:512, :600, :3]
+
+    augmentations = config.both_transform(image=input_image, image0=target_image)
+    input_image = augmentations["image"]
+    target_image = augmentations["image0"]
+
+    target_image = config.transform_only_mask(image=target_image)["image"]
+    input_image = input_image[None, :, :, :]
+    target_image = target_image[None, :, :, :]
+    path = "./maps_model/gen.pth.tar"
+    model = load_model(path)
+    model.eval()
+
+    col1, col2 = st.columns(2)
+    if options:
+        with col1:
+            img_path = os.path.join('maps', 'images', selected_photo)
+            image = np.array(Image.open(img_path))
+
+            input_image = image[:512, 512:, :3]
+            input_image = input_image[None, :, :, :]
+            st.image(input_image)
+
+        with col2:
+            with torch.no_grad():
+                y_fake = model(target_image)
+                y_fake = y_fake * 0.5 + 0.5
+                y = torch.squeeze(y_fake)
+                fig, ax = plt.subplots()
+                ax = plt.imshow(y.permute(1, 2, 0))
+                st.pyplot(fig)
+    
