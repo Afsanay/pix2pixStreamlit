a65db34b7225e8dd2e0ec547ade3348428ffe491
Afsanay <priyanshusobti02@gmail.com>
2023-06-27 01:32:53 +0530

    newgen

--- streamlitapp.py
+++ streamlitapp.py
@@ -58,3 +58,61 @@ if model_type == "anime":
                 fig, ax = plt.subplots()
                 ax = plt.imshow(y.permute(1, 2, 0))
                 st.pyplot(fig)
+                
+else:        
+    options = os.listdir(os.path.join('anime', 'images'))
+    selected_photo = st.selectbox("Choose a photo", options=options)
+
+    img_path = os.path.join('anime', 'images', selected_photo)
+    image = np.array(Image.open(img_path))
+
+    input_image = image[:512, 512:, :3]
+    target_image = image[:512, 512:, :3]
+
+    augmentations = config.both_transform(image=input_image, image0=target_image)
+    input_image = augmentations["image"]
+    target_image = augmentations["image0"]
+
+    target_image = config.transform_only_mask(image=target_image)["image"]
+    input_image = input_image[None, :, :, :]
+    target_image = target_image[None, :, :, :]
+    path = "gen2.pth.tar"
+    model = load_model(path)
+    model.eval()
+
+    col1, col2 = st.columns(2)
+    if options:
+        with col1:
+            img_path = os.path.join('anime', 'images', selected_photo)
+            image = np.array(Image.open(img_path))
+
+            input_image = image[:512, 512:, :3]
+            input_image = input_image[None, :, :, :]
+            st.image(input_image)
+
+        with col2:
+            with torch.no_grad():
+                y_fake = model(target_image)
+                y_fake = y_fake * 0.5 + 0.5
+                y = torch.squeeze(y_fake)
+                fig, ax = plt.subplots()
+                ax = plt.imshow(y.permute(1, 2, 0))
+                st.pyplot(fig)
+                      
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
